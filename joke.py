8ae0bce9ddbee9f56917be0086b9920eca6ae401
Silkywilky95 <silkywilky9568@gmail.com>
2024-08-26 13:47:23 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,7 @@
+print("knock, knock.")
+print("who's there?")
+print ("boo")
+print ("boo hoo?")
+print ("dont cry, its just a joke")
+#Assignment Knock Knock Joke/ As one line of code 
+print("Knock, knock.\nWho's there?\nboo\nboo hoo?\nDont cry its just a joke")

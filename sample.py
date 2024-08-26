fa1dcd6a478928a5b54d2b0b29ebf7e6129bce51
Silkywilky95 <silkywilky9568@gmail.com>
2024-08-26 14:44:28 -0500

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 2)
+
+# Integer
+y = -10
+print("Integer:", y)
+
+# Float
+z = 15.5
+print("Float:", z)
+
+# String
+name = "Andrew"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)

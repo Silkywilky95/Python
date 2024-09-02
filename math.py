995678b208c45b6dd95c594426274b9d2910aacd
Silkywilky95 <silkywilky9568@gmail.com>
2024-09-02 15:53:37 -0500

    Add files via upload

---
+++ math.py
@@ -0,0 +1,34 @@
+# Addition
+sum = 10 + 20
+print("\nAddition: ")
+print(30)
+
+# Subtraction
+diff = 70 - 30
+print("\nSubtraction: ")
+print(40)
+
+# Multiplication
+product = 10 * 10
+print("\nMultiplication: ")
+print(100)
+
+# Division
+quotient = 60 / 30
+print("\nDivision: ")
+print(2)
+
+# Integer Division
+integer_div = 7 // 3
+print("\nInteger Division: ")
+print(2)
+
+# Modulus
+remainder = 14 % 5
+print("\nModulus: ")
+print(4)
+
+# Exponentiation
+exp = 3 ** 4
+print("\nExponentiation:")
+print(81)
